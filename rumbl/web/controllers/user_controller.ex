5ee7b50d4ddcdfe83a308bec57856dfb5a88d0fb
4pcbr <me@4pcbr.com>
2016-01-11 23:26:14 +0100

    Some more html templating

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  require Logger
   
   def index( conn, _params ) do
     users = Repo.all( Rumbl.User )
@@ -7,7 +8,9 @@ defmodule Rumbl.UserController do
   end
 
   def show( conn, %{ "id" => id }) do
+    Logger.debug inspect( id )
     user = Repo.get( Rambl.User, id )
+    Logger.debug inspect( user )
     render conn, "show.html", user: user
   end
 
