3a4504f2c7711c8c1986fab7c325fcf7886e3d31
4pcbr <me@4pcbr.com>
2016-02-04 23:56:44 +0100

    Video slugs + Permalink

--- rumbl/web/models/video.ex
+++ rumbl/web/models/video.ex
@@ -1,6 +1,7 @@
 defmodule Rumbl.Video do
   use Rumbl.Web, :model
 
+  @primary_key { :id, Rumbl.Permalink, autogenerate: true }
   schema "videos" do
     field :url, :string
     field :title, :string
@@ -42,5 +43,11 @@ defmodule Rumbl.Video do
       |> String.replace( ~r/[^\w-]+/, "-" )
   end
 
-
 end
+
+defimpl Phoenix.Param, for: Rumbl.Video do
+  def to_param(%{ slug: slug, id: id }) do
+    "#{id}-#{slug}"
+  end
+end
+
